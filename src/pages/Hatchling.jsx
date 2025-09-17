b0dd675011821c97d88fdcfba98e762a3ee03d9a
Saksham <89475660+Cla5sic@users.noreply.github.com>
2025-09-17 16:40:18 -0500

    Update Hatchling.jsx

--- src/pages/Hatchling.jsx
+++ src/pages/Hatchling.jsx
@@ -142,7 +142,7 @@ const Hatchling = () => {
           <div className="rounded-2xl overflow-hidden border border-gray-200/50 dark:border-gray-700/50 glass-card" style={{ height: '400px' }}>
             <ImageCarousel 
               images={[
-                '<blockquote class="imgur-embed-pub" lang="en" data-id="OFjrpq6"><a href="https://imgur.com/OFjrpq6">View post on imgur.com</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>,
+                'https://imgur.com/OFjrpq6',
                 'https://picsum.photos/seed/hatchling2/1200/600',
                 'https://picsum.photos/seed/hatchling3/1200/600',
                 'https://picsum.photos/seed/hatchling4/1200/600',
