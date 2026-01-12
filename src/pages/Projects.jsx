78c1541fa3536dec43596656c0012e5506cbe06a
Saksham <saksham.mohan@gmail.com>
2026-01-11 22:25:09 -0600

    Made grid size adapt to device size

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -209,7 +209,7 @@ const Projects = () => {
       {/* Projects Grid */}
       <section className="py-12 px-4">
         <div className="max-w-7xl mx-auto">
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
             {filteredProjects.map((project, index) => (
               <div 
                 key={index}
@@ -284,4 +284,4 @@ const Projects = () => {
   )
 }
 
-export default Projects 
+export default Projects
