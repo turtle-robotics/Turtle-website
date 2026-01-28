bdba5ce1ae9eac2824a09668b8b9a1793424ba8d
Fiszic <zhengyechen2018@gmail.com>
2026-01-28 16:55:12 -0600

    Fix for hatchling page to no longer crash.

--- src/pages/Hatchling.jsx
+++ src/pages/Hatchling.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef , useState} from "react";
 import { gsap } from "gsap";
 import ImageCarousel from "../components/ImageCarousel";
 import ButtonLink from "../components/ButtonLink";
