af62728d5c88e2dc0ef559aef01e0675c12e3661
Micah Guttman <memtech3@gmail.com>
2026-02-04 02:40:27 -0600

    removed dark mode stuff from css files and docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -15,26 +14,21 @@ import Showcase from "./pages/Showcase";
 
 function App(Component, pageProps) {
   return (
-    <ThemeProvider>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:projectId" element={<ProjectDetail />} />
-          <Route
-            path="/development-programs"
-            element={<DevelopmentPrograms />}
-          />
-          <Route path="/hatchling" element={<Hatchling />} />
-          <Route path="/workshop-series" element={<WorkshopSeries />} />
-          <Route path="/showcase" element={<Showcase />} />
-          <Route path="/apply" element={<Apply />} />
-          <Route path="/sponsorship" element={<Sponsorship />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:projectId" element={<ProjectDetail />} />
+        <Route path="/development-programs" element={<DevelopmentPrograms />} />
+        <Route path="/hatchling" element={<Hatchling />} />
+        <Route path="/workshop-series" element={<WorkshopSeries />} />
+        <Route path="/showcase" element={<Showcase />} />
+        <Route path="/apply" element={<Apply />} />
+        <Route path="/sponsorship" element={<Sponsorship />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 }
 
